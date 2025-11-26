4939eac5be168ead03d39e255e6d6bc86d64a723
Yukio <mrtracerdev@gmail.com>
2025-11-26 15:08:36 +0100

    feat (components): Added stickynote component

---
+++ components/StickyNote.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+type StickyNoteProps = {
+  children: ReactNode;
+  className?: string;
+  rotate?: boolean;
+};
+
+// Einfache Sticky-Note (gelb, leicht rotiert (optional))
+export function StickyNote({
+  children,
+  className = "",
+  rotate = true,
+}: StickyNoteProps) {
+  return (
+    <div
+      className={`bg-sticky text-text font-handwritten shadow-stickynote font-semibold text-center text-xl px-4 py-3 ${
+        rotate ? "rotate-2" : ""
+      } ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
