82324c42de6414e3e8c66dc4cc0a69a647931a24
Yukio <mrtracerdev@gmail.com>
2025-12-07 16:03:56 +0100

    chore (robots): Base URLs are now robust

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,10 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || "https://test.com";
+  const base =
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    process.env.SITE_URL ||
+    "http://localhost:3000";
 
   return {
     rules: [
